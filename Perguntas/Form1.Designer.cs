6aade5137c31bf04ce7fdfe50e7c18ead0b65cb2
Rafael Seidi Suzuki Tomita <100170190+RafaelSeidi@users.noreply.github.com>
2022-03-14 20:08:02 -0300

    Add files via upload

---
+++ Perguntas/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Perguntas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(393, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(177, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(79, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Abrir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(531, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 72);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sortear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(396, 120);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(32, 33);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "A";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(396, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "B";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(396, 198);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(32, 33);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "C";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(396, 237);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(32, 33);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "D";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(434, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(434, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(434, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 70);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(375, 275);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Magneto", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label7.Location = new System.Drawing.Point(96, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(623, 58);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Easter egg do Milhão!!";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 275);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 414);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Perguntas/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perguntas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ok;
+        Random rnd = new Random(8);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.LoadFile("WWW.txt", RichTextBoxStreamType.PlainText);
+            label1.Text = "";
+            button3.Enabled = true;
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SaveFile("WWW.txt",RichTextBoxStreamType.PlainText);
+            label1.Text = "Arquivo Salvado!!!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            habilitar(false);
+            pictureBox1.Visible = false;
+            button1.Enabled = false;
+        }
+        private void habilitar (Boolean oq)
+        {
+            button4.Enabled = oq;
+            button5.Enabled = oq;
+            button6.Enabled = oq;
+            button7.Enabled = oq;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            habilitar(false);
+
+            if (ok.ToUpper() == "A")
+            {
+                label1.Text = "Parabéns! Acertoooou!!";
+                pictureBox1.Load("acertou.gif");
+            }
+            else
+            {
+                label1.Text = "Putz! Errooooooou!!";
+                pictureBox1.Load("errou.gif");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = false;
+            habilitar(true);
+            label1.Text = "";
+
+            int qual = rnd.Next(richTextBox1.Lines.Length);
+
+            string[] linha = richTextBox1.Lines[qual].Split('@');
+
+            label2.Text = qual + ".";
+
+            label2.Text = linha[0];
+            label3.Text = linha[1];
+            label4.Text = linha[2];
+            label5.Text = linha[3];
+            label6.Text = linha[4];
+            ok = linha[5];
+
+            pictureBox1.Visible = true;
+            pictureBox1.Load("Showdomilhaosbt.jpg");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            habilitar(false);
+
+            if (ok.ToUpper() == "B")
+            {
+                label1.Text = "Parabéns! Acertoooou!!";
+                pictureBox1.Load("acertou.gif");
+            }
+            else
+            {
+                label1.Text = "Putz! Errooooooou!!";
+                pictureBox1.Load("errou.gif");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            habilitar(false);
+
+            if (ok.ToUpper() == "C")
+            {
+                label1.Text = "Parabéns! Acertoooou!!";
+                pictureBox1.Load("acertou.gif");
+            }
+            else
+            {
+                label1.Text = "Putz! Errooooooou!!";
+                pictureBox1.Load("errou.gif");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            habilitar(false);
+
+            if (ok.ToUpper() == "D")
+            {
+                label1.Text = "Parabéns! Acertoooou!!";
+                pictureBox1.Load("acertou.gif");
+            }
+            else
+            {
+                label1.Text = "Putz! Errooooooou!!";
+                pictureBox1.Load("errou.gif");
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
